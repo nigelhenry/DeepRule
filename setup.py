b6059e46a9e12ef591d4346828a08111fa25f785
Nigel Henry <nigelhenry@gmail.com>
2023-03-28 12:05:49 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='deeprule',
     version='0.1',
-    package_dir={"DeepRule"},
     packages=find_packages(),
     include_package_data=True
 )
