5a7cfb93e8619d46e1b3fc2f29bd81696c64333c
Nigel Henry <nigelhenry@microsoft.com>
2023-03-29 19:00:57 -0400

    bug fix 2

--- DeepRule/models/py_utils/_cpools/__init__.py
+++ DeepRule/models/py_utils/_cpools/__init__.py
@@ -12,8 +12,8 @@ def load_lib(libname):
     #libdir = os.path.dirname(__file__)
     libdir = "."
     libpath = os.path.join(libdir, libname)
-#    os.listdir(libpath)
-    return ctypes.cdll.LoadLibrary(libpath)
+    os.listdir(libdir)
+#    return ctypes.cdll.LoadLibrary(libpath)
 
 top_pool = load_lib('top_pool.cpython-36m-x86_64-linux-gnu.so')
 bottom_pool = load_lib('bottom_pool.cpython-36m-x86_64-linux-gnu.so')
