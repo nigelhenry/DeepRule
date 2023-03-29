8c3a7150b2cc3836ad75605ba06e08c1c5eacef4
Nigel Henry <nigelhenry@microsoft.com>
2023-03-29 17:51:49 -0400

    top pool take 2

--- DeepRule/models/py_utils/_cpools/top_pool.py
+++ DeepRule/models/py_utils/_cpools/top_pool.py
@@ -1,8 +1,16 @@
-def __bootstrap__():
+#def __bootstrap__():
 #    global __bootstrap__, __loader__, __file__
-    import sys, pkg_resources, imp
+#    import sys, pkg_resources, imp
 #    __file__ = pkg_resources.resource_filename(__name__, 'top_pool.cpython-36m-x86_64-linux-gnu.so')
 #    __loader__ = None; del __bootstrap__, __loader__
 #    imp.load_dynamic(__name__,__file__)
-    imp.load_dynmaic(__name__,"top_pool.cpython-36m-x86_64-linux-gnu.so")
-__bootstrap__()
+#__bootstrap__()
+import os
+import ctypes
+
+def load_lib(libname):
+    libdir = os.path.dirname(__file__)
+    libpath = os.path.join(libdir, libname)
+    return ctypes.cdll.LoadLibrary(libpath)
+
+top_pool = load_lib('top_pool.cpython-36m-x86_64-linux-gnu.so')
