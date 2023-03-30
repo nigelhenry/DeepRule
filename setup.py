ebed2f7a9686ca1cee379bee6890abd4ae8868f2
Nigel Henry <nigelhenry@gmail.com>
2023-03-30 14:12:49 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='deeprule',
     version='0.1',
-    package_data={"": ["models/py_utils/_cpools/src/*.cpp"]},
+    package_data={"": ["models/py_utils/_cpools/src/*.cpp",
+                      "pycocotool/_mask.cpython-36m-x86_64-linux-gnu.so"]},
     include_package_data=True
 )
