eab850e0c1cfc965027e3491a6bbc68ab9f1dd4e
Nigel Henry <nigelhenry@gmail.com>
2023-03-30 11:11:56 -0400

    Update top_pool.cpp

--- DeepRule/models/py_utils/_cpools/src/top_pool.cpp
+++ DeepRule/models/py_utils/_cpools/src/top_pool.cpp
@@ -2,27 +2,27 @@
 
 #include <vector>
 
-std::vector<at::Tensor> top_pool_forward(
-    at::Tensor input
+std::vector<torch::Tensor> top_pool_forward(
+    torch::Tensor input
 ) {
     // Initialize output
-    at::Tensor output = at::zeros_like(input);
+    torch::Tensor output = torch::zeros_like(input);
 
     // Get height
     int64_t height = input.size(2);
 
     // Copy the last column
-    at::Tensor input_temp  = input.select(2, height - 1);
-    at::Tensor output_temp = output.select(2, height - 1);
+    torch::Tensor input_temp  = input.select(2, height - 1);
+    torch::Tensor output_temp = output.select(2, height - 1);
     output_temp.copy_(input_temp);
 
-    at::Tensor max_temp;
+    torch::Tensor max_temp;
     for (int64_t ind = 1; ind < height; ++ind) {
         input_temp  = input.select(2, height - ind - 1);
         output_temp = output.select(2, height - ind);
         max_temp    = output.select(2, height - ind - 1);
 
-        at::max_out(max_temp, input_temp, output_temp);
+        torch::max_out(max_temp, input_temp, output_temp);
     }
 
     return { 
@@ -30,19 +30,19 @@ std::vector<at::Tensor> top_pool_forward(
     };
 }
 
-std::vector<at::Tensor> top_pool_backward(
-    at::Tensor input,
-    at::Tensor grad_output
+std::vector<torch::Tensor> top_pool_backward(
+    torch::Tensor input,
+    torch::Tensor grad_output
 ) {
-    auto output = at::zeros_like(input);
+    auto output = torch::zeros_like(input);
 
     int32_t batch   = input.size(0);
     int32_t channel = input.size(1);
     int32_t height  = input.size(2);
     int32_t width   = input.size(3);
 
-    auto max_val = at::zeros(torch::CUDA(at::kFloat), {batch, channel, width});
-    auto max_ind = at::zeros(torch::CUDA(at::kLong),  {batch, channel, width});
+    auto max_val = torch::zeros(torch::CUDA(torch::kFloat), {batch, channel, width});
+    auto max_ind = torch::zeros(torch::CUDA(torch::kLong),  {batch, channel, width});
 
     auto input_temp = input.select(2, height - 1);
     max_val.copy_(input_temp);
@@ -54,13 +54,13 @@ std::vector<at::Tensor> top_pool_backward(
     output_temp.copy_(grad_output_temp);
 
     auto un_max_ind = max_ind.unsqueeze(2);
-    auto gt_mask    = at::zeros(torch::CUDA(at::kByte),  {batch, channel, width});
-    auto max_temp   = at::zeros(torch::CUDA(at::kFloat), {batch, channel, width});
+    auto gt_mask    = torch::zeros(torch::CUDA(torch::kByte),  {batch, channel, width});
+    auto max_temp   = torch::zeros(torch::CUDA(torch::kFloat), {batch, channel, width});
     for (int32_t ind = 1; ind < height; ++ind) {
         input_temp = input.select(2, height - ind - 1);
-        at::gt_out(gt_mask, input_temp, max_val);
+        torch::gt_out(gt_mask, input_temp, max_val);
 
-        at::masked_select_out(max_temp, input_temp, gt_mask);
+        torch::masked_select_out(max_temp, input_temp, gt_mask);
         max_val.masked_scatter_(gt_mask, max_temp);
         max_ind.masked_fill_(gt_mask, height - ind - 1);
 
