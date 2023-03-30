c3e03b0088d5ccdb60d1967bf17663d633df5592
Nigel Henry <nigelhenry@microsoft.com>
2023-03-30 15:34:42 -0400

    added dir to setup.py

--- DeepRule/pycocotool/setup.py
+++ DeepRule/pycocotool/setup.py
@@ -4,10 +4,12 @@ import numpy as np
 # To compile and install locally run "python setup.py build_ext --inplace"
 # To install library to Python site-packages run "python setup.py build_ext install"
 
+libdir = os.path.dirname(__file__)
+
 ext_modules = [
     Extension(
         '_mask',
-        sources=['_mask.c', '_mask.pyx'],
+        sources=[libdir+'/_mask.c', libdir+'/_mask.pyx'],
         include_dirs = [np.get_include(), '.'],
         extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
     )
