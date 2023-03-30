1d154cc2f451250fd815dd86f078ae7e450e579d
Nigel Henry <nigelhenry@gmail.com>
2023-03-30 13:59:00 -0400

    Update __init__.py

--- DeepRule/models/py_utils/_cpools/__init__.py
+++ DeepRule/models/py_utils/_cpools/__init__.py
@@ -2,10 +2,9 @@ import torch
 
 from torch import nn
 from torch.autograd import Function
-#from models.py_utils._cpools import top_pool, bottom_pool, left_pool, right_pool
 
 import os
-import ctypes
+#import ctypes
 
 libdir = os.path.dirname(__file__)
 os.system(f"python {libdir}/setup.py build_ext --inplace")
@@ -18,10 +17,12 @@ def load_lib(libname):
         raise FileNotFoundError(f"{libpath} not found in current directory.")
     return ctypes.cdll.LoadLibrary("./"+libpath)
 
-top_pool = load_lib('top_pool.cpython-37m-x86_64-linux-gnu.so')
-bottom_pool = load_lib('bottom_pool.cpython-37m-x86_64-linux-gnu.so')
-left_pool = load_lib('left_pool.cpython-37m-x86_64-linux-gnu.so')
-right_pool = load_lib('right_pool.cpython-37m-x86_64-linux-gnu.so')
+from models.py_utils._cpools import top_pool, bottom_pool, left_pool, right_pool
+
+#top_pool = load_lib('top_pool.cpython-37m-x86_64-linux-gnu.so')
+#bottom_pool = load_lib('bottom_pool.cpython-37m-x86_64-linux-gnu.so')
+#left_pool = load_lib('left_pool.cpython-37m-x86_64-linux-gnu.so')
+#right_pool = load_lib('right_pool.cpython-37m-x86_64-linux-gnu.so')
 
 class TopPoolFunction(Function):
     @staticmethod
