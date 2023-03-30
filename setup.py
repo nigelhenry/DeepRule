f1192cb15b5e853dda5328bf831d00070bbcde6f
Nigel Henry <nigelhenry@gmail.com>
2023-03-30 14:06:00 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='deeprule',
     version='0.1',
-    package_data={"": ["DeepRule/models/py_utils/_cpools/src/*.cpp","models/py_utils/_cpools/src/*.cpp"]},
+    package_data={"": ["models/py_utils/_cpools/src/*.cpp"]},
     include_package_data=True
 )
