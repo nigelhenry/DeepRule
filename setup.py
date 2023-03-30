dc0b1f19b6463932e24e2dcd55c56114c016040d
Nigel Henry <nigelhenry@gmail.com>
2023-03-30 06:22:47 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name='deeprule',
     version='0.1',
     include_package_data=True,
-    package_data={"": ["DeepRule/models/py_utils/_cpools/*.so"]}
+    package_data={"": ["models/py_utils/_cpools/*.so"]}
 )
