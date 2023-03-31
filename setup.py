078bb79dcd9d6249e62fbaf3412db4eacbfef56e
Nigel Henry <nigelhenry@gmail.com>
2023-03-30 23:46:55 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='deeprule',
     version='0.1',
     package_data={"": ["models/py_utils/_cpools/src/*.cpp",
-                      "pycocotool/_mask.pyx"]},
+                      "pycocotool/_mask.pyx",
+                      "pycocotool/maskApi.h"]},
     include_package_data=True
 )
